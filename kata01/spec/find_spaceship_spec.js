844f188e92e02ae96fa5a9f4fef824006edf7308
mosupercode <mo.supercode@gmail.com>
2024-05-16 12:39:53 +0200

    2. Todo - Red

--- kata01/spec/find_spaceship_spec.js
+++ kata01/spec/find_spaceship_spec.js
@@ -4,4 +4,10 @@ describe("Find Spaceship", function () {
     const result = findSpaceship(map);
     expect(result).toEqual(3);
   });
+
+  it('should return noString  for "100" ', function () {
+    const map = 100;
+    const result = findSpaceship(map);
+    expect(result).toEqual("no String");
+  });
 });
