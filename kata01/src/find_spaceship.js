2ce0a2dffdd886da3c35548a2b7b241d72bc0de3
mosupercode <mo.supercode@gmail.com>
2024-05-16 15:38:00 +0200

    5.Todo - refactor, console.logs entfernt.

--- kata01/src/find_spaceship.js
+++ kata01/src/find_spaceship.js
@@ -5,13 +5,12 @@ function findSpaceship(map) {
   let result = map.includes("x") ? "x found" : "Spaceship lost forever";
 
   const rows = map.split("\n").reverse();
-  console.log(rows);
   for (let i = 0; i < rows.length; i++) {
     const x = rows[i].indexOf("x");
     if (x !== -1) {
+      // -1 zeigt an, dass das Zeichen nicht gefunden wurde, also wurde das x mit seiner position gefunden
       result += `[${i},${x}]`;
     }
   }
-  console.log(result);
   return result;
 }
