b714650f8a4e8ca44f66bed523e48b908da8a3c4
mosupercode <mo.supercode@gmail.com>
2024-05-16 12:30:47 +0200

    First Todo - red

---
+++ kata01/spec/find_spaceship_spec.js
@@ -0,0 +1,7 @@
+describe("Find Spaceship", function () {
+  it('should return string length for "..." ', function () {
+    const map = "...";
+    const result = findSpaceship(map);
+    expect(result.toEqual(3));
+  });
+});
---
+++ kata01/src/find_spaceship.js
@@ -0,0 +1,1 @@
+function findSpaceship(map) {}
