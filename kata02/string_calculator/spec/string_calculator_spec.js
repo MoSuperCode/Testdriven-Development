6a2aaec752fcb7cbc83628fdc22cd64068ac6f80
mosupercode <mo.supercode@gmail.com>
2024-06-02 22:01:18 +0200

    kata02 - Second Todo - Red Commit

--- kata02/string_calculator/spec/string_calculator_spec.js
+++ kata02/string_calculator/spec/string_calculator_spec.js
@@ -7,4 +7,9 @@ describe("Electrons around the World", function () {
     const result = elektrons.add([1, 2, 3, 4, 5]);
     expect(result).toEqual(6);
   });
+
+  it("should return 4 for this input: [ 2, 2, 3, 3 ]", function () {
+    const result = elektrons.add([2, 2, 3, 3]);
+    expect(result).toEqual(4);
+  });
 });
