4e0d066779dc7f892b7c7b12c574c719a2ecf664
mosupercode <mo.supercode@gmail.com>
2024-06-02 22:08:56 +0200

    kata02 - Third Todo - green Commit

--- kata02/string_calculator/src/string_calculator.js
+++ kata02/string_calculator/src/string_calculator.js
@@ -3,8 +3,12 @@ function electroAroundTheWorld(inputArray) {
   this.inputArray = inputArray;
 }
 electroAroundTheWorld.prototype.add = function (inputArray) {
-  inputArray.length > 4
-    ? (inputArray = inputArray.length + 1)
-    : (inputArray = inputArray.length);
-  return inputArray;
+  if (inputArray.includes(6)) {
+    return (inputArray = 2);
+  } else {
+    inputArray.length > 4
+      ? (inputArray = inputArray.length + 1)
+      : (inputArray = inputArray.length);
+    return inputArray;
+  }
 };
