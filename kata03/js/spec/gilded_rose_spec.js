fc29c00f93e65dad28d6842aa755259240bb4888
mosupercode <mo.supercode@gmail.com>
2024-06-10 12:59:10 +0200

    Kata03 - third Unit Test - green, item quality sinkt nach anwenden der update quality methode

--- kata03/js/spec/gilded_rose_spec.js
+++ kata03/js/spec/gilded_rose_spec.js
@@ -36,7 +36,7 @@ describe("Gilded Rose", function () {
   });
   it("should degrade normal items before sell date", function () {
     update_quality();
-    expect(items[0].quality).toEqual("fixme");
-    expect(items[0].sell_in).toEqual("fixme");
+    expect(items[0].quality).toEqual(19);
+    expect(items[0].sell_in).toEqual(9);
   });
 });
