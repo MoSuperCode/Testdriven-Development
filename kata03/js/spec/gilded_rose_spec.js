3de3b2394f78a5ac881947d7785beebde377cb9e
mosupercode <mo.supercode@gmail.com>
2024-06-10 13:23:48 +0200

    Kata03 - 4th Unit test- red

--- kata03/js/spec/gilded_rose_spec.js
+++ kata03/js/spec/gilded_rose_spec.js
@@ -39,4 +39,10 @@ describe("Gilded Rose", function () {
     expect(items[0].quality).toEqual(19);
     expect(items[0].sell_in).toEqual(9);
   });
+  it("should degrade normal items twice as fast after sell date", function () {
+    items[1].sell_in = 0;
+    update_quality();
+    expect(items[1].quality).toEqual(0);
+    expect(items[1].sell_in).toEqual(-1);
+  });
 });
